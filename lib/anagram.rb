661955f40dbcb8c00aa44f6c052d5db2bdc94123
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-03 01:15:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,7 +4,8 @@ class Anagram
   
   def initialize(word)
     @word = word
-    %w(&:anagrams_spec.rb).find_all {|w| w.split("").sort == @word.split("").sort}
+    array = %w(hello world zombies pants dipper)
+    array.find_all {|w| w.split("").sort == @word.split("").sort}
   end
   
   
