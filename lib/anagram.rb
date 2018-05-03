7eebd7be777001cbf603bbd8b769b6cb068ddebc
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-03 00:51:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,8 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :
+  attr_accessor :word
   def initialize(word)
     word.split("").sort
     %w(anagrams_spec.rb).find_all {|words| words == word}
   end
-    
-  end 
+end 
